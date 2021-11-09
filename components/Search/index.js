98a74de767edfd55bc720284d681c840ce160e76
arian457 <ariancejas00@gmail.com>
2021-11-09 10:24:30 -0300

    connected searchBar to context

--- components/Search/index.js
+++ components/Search/index.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useContext } from "react";
+import AppContext from "../../AppContext";
+import { getService } from "../../services";
 import styles from "./search.module.css";
 
 export const Search = () => {
-    return (
-        <div className={styles.searchContainer}>
-            <h1>Find your favorites Artist!</h1>
-            <input type="text" />
-        </div>
-    )
-}
+  const value = useContext(AppContext);
+  const { setLoading, setArtists } = value;
+  const handleChange = async (e) => {
+    setLoading(true);
+    if (e.target.value) {
+      const request = await getService("/spotify/artist", {
+        artist_name: e.target.value,
+      });
+      setArtists(request.data);
+    } else setArtists([]);
+    setLoading(false);
+  };
+  return (
+    <div className={styles.searchContainer}>
+      <h1>Find your favorites Artist!</h1>
+      <input onChange={(e) => handleChange(e)} type="text" />
+    </div>
+  );
+};
