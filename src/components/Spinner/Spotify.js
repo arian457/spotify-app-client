484e3b0815f3d86326ad029ede8213c119fbf62f
arian457 <ariancejas00@gmail.com>
2021-11-10 21:47:56 -0300

    [Spinner] spotify

---
+++ src/components/Spinner/Spotify.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react'
+import lottie from "lottie-web/build/player/lottie_light";
+import spotifyAnimation from "/public/spotify.json";
+
+export const Spinner = () => {
+    const [animation, setAnimation] = useState(false);
+  useEffect(() => {
+    if (!animation) {
+      lottie.loadAnimation({
+        container: document.querySelector("#spotify-logo"),
+        animationData: spotifyAnimation,
+      });
+      setAnimation(!animation);
+    }
+  }, [animation]);
+    return (
+        <div id="spotify-logo" style={{width:"200px"}} />
+    )
+}
